969fc607f3b169b7dddca8f0764d6e5c172eacd6
Yasasi Kariyawasam <158291905+YasasiKariyawasam@users.noreply.github.com>
2026-05-02 23:43:06 +0530

    added report generator

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,11 +1,11 @@
 from .coordinator_agent import coordinator_node
 from .code_analyzer_agent import code_analyzer_node
 from .security_scanner_agent import security_scanner_node
-# from .report_generator_agent import report_generator_node
+from .report_generator_agent import report_generator_node
 
 __all__ = [
     "coordinator_node",
     "code_analyzer_node",
     "security_scanner_node",
-    # "report_generator_node",
+    "report_generator_node",
 ]
